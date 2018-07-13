c3115b51aaa152e95db0507457e8aebc359ff768
Eric Mason <m.mason@wustl.edu>
2018-07-13 09:26:38 -0500

    adding links in sponsor column and fixing size of session image location

--- api/models/session.py
+++ api/models/session.py
@@ -4,7 +4,7 @@ class Session(models.Model):
 
     title = models.CharField(max_length=63, blank=True)
     session_type = models.CharField(max_length=63, blank=True)
-    image_loc = models.CharField(max_length=63, blank=True)
+    image_loc = models.CharField(max_length=255, blank=True)
     description = models.CharField(max_length=255, blank=True)
     time = models.DateTimeField(blank=True, null=True)
     room_num = models.PositiveSmallIntegerField(blank=True, null=True)
