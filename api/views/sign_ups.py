d9bd094b419b22b96991a9de9c54d8aa4f3b3a90
Daniel Borstelmann <dborstelmann@wustl.edu>
2018-07-23 17:07:08 -0500

    ugh

--- api/views/sign_ups.py
+++ api/views/sign_ups.py
@@ -2,14 +2,12 @@ from api.models import EmailSignUp
 from api.models import SponsorQuery
 from django.views.generic.base import View
 from django.http import HttpResponseBadRequest
-from django.views.decorators.csrf import csrf_exempt
 from utils import json_response
 import json
 
 
 class EmailSignUpView(View):
     @staticmethod
-    @csrf_exempt
     def post(request):
         params = json.loads(request.body)
         email = params.get('email', '')
@@ -26,7 +24,6 @@ class EmailSignUpView(View):
 
 class SponsorQueryView(View):
     @staticmethod
-    @csrf_exempt
     def post(request):
         params = json.loads(request.body)
         email = params.get('email', '')
