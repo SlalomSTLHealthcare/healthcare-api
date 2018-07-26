ec0829d0c0138314545715456a48f5a39e5f821e
sorayamoss <sorayamoss@wustl.edu>
2018-07-26 09:12:01 -0500

    learned a lot along the way

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from .authentication import stlx_login, stlx_logout, stlx_register
+from .authentication import stlx_login, stlx_logout, stlx_register, stlx_profile
 
 urlpatterns = [
     url(r'^login', stlx_login, name='login'),
     url(r'^logout', stlx_logout, name='logout'),
-    url(r'^register', stlx_register, name='register')
+    url(r'^register', stlx_register, name='register'),
+    url(r'^profile', stlx_profile, name='profile')
 ]
