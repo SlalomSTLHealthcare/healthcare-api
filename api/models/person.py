52a3305f190b032e739d2cbe3f7bdab963ebbc35
Daniel Borstelmann <dborstelmann@wustl.edu>
2018-08-22 17:30:15 -0500

    yolo

--- api/models/person.py
+++ api/models/person.py
@@ -1,10 +1,12 @@
 from django.db import models
 from api.models import Session
 
+
 class Person(models.Model):
 
     full_name = models.CharField(max_length=63, blank=True)
-    bio = models.CharField(max_length=255, blank=True)
+    title = models.CharField(max_length=255, blank=True)
+    bio = models.CharField(max_length=1023, blank=True)
     company = models.CharField(max_length=63, blank=True)
     image_loc = models.CharField(max_length=255, blank=True)
     session = models.ManyToManyField(Session)
