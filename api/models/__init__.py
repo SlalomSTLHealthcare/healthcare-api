80a824f0ac5b509a4d8bfc098a74364744755bac
Daniel Borstelmann <dborstelmann@wustl.edu>
2018-08-22 23:30:41 -0500

    merge flix

--- api/models/__init__.py
+++ api/models/__init__.py
@@ -3,9 +3,6 @@ from .person import Person
 from .schedule import Schedule
 from .sponsor import Sponsor
 from .attendee import Attendee
-<<<<<<< HEAD
-from .attendee import Session_Attendee
-=======
 from .email_sign_up import EmailSignUp
 from .sponsor_query import SponsorQuery
->>>>>>> master
+from .attendee import Session_Attendee
