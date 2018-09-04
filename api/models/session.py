698b0f1deef65519d7b6f98251cce0e697e8fb23
Daniel Borstelmann <dborstelmann@wustl.edu>
2018-09-04 13:26:16 -0500

    yolo

--- api/models/session.py
+++ api/models/session.py
@@ -8,7 +8,7 @@ class Session(models.Model):
     image_loc = models.CharField(max_length=255, blank=True)
     description = models.CharField(max_length=1023, blank=True)
     time = models.DateTimeField(blank=True, null=True)
-    room_num = models.PositiveSmallIntegerField(blank=True, null=True)
+    room_num = models.CharField(max_length=63, blank=True, null=True)
     schedule = models.ForeignKey(Schedule, on_delete=models.PROTECT, related_name='sessions', null=True)
     max_capacity = models.PositiveSmallIntegerField(blank=True, null=True)
 
