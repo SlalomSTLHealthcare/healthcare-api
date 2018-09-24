22749f9d3b01903451df027e5a9b0bee8154cb81
Daniel Borstelmann <dborstelmann@wustl.edu>
2018-09-24 10:48:36 -0500

    bio length

--- api/models/person.py
+++ api/models/person.py
@@ -6,7 +6,7 @@ class Person(models.Model):
 
     full_name = models.CharField(max_length=63, blank=True)
     title = models.CharField(max_length=255, blank=True)
-    bio = models.CharField(max_length=1023, blank=True)
+    bio = models.CharField(max_length=2047, blank=True)
     company = models.CharField(max_length=63, blank=True)
     image_loc = models.CharField(max_length=255, blank=True)
     session = models.ManyToManyField(Session, blank=True)
